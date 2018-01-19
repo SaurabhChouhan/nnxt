d1a09d467a5e0afede6c24d94813cdb40222e591
Saurabh Chouhan <schouhan@aripratech.com>
2018-01-19 22:17:18 +0530

    Added method to check if logged in user has a particular role

--- server/utils.js
+++ server/utils.js
@@ -1,4 +1,8 @@
-import {ROLE_ADMIN, ROLE_SUPER_ADMIN, ROLE_APP_USER} from "./serverconstants"
+import {
+    ROLE_ADMIN,
+    ROLE_SUPER_ADMIN,
+    ROLE_APP_USER
+} from "./serverconstants"
 
 export const isAuthenticated = (ctx) => {
     if (ctx.isAuthenticated())
@@ -25,6 +29,14 @@ export const isAdmin = (ctx) => {
 export const isAppUser = (ctx) => {
     if (ctx.isAuthenticated()) {
         if (ctx.state.user && Array.isArray(ctx.state.user.roles) && ctx.state.user.roles.findIndex(r => r.name == ROLE_APP_USER) != -1)
+            return true
+    }
+    return false
+}
+
+export const hasRole = (ctx, roleName) => {
+    if (ctx.isAuthenticated()) {
+        if (ctx.state.user && Array.isArray(ctx.state.user.roles) && ctx.state.user.roles.findIndex(r => r.name == roleName) != -1)
             return true
     }
     return false
