d8d915ab6cb522cfb86487b32e24403491241a95
Saurabh Chouhan <schouhan@aripratech.com>
2018-01-23 17:57:49 +0530

    Added fonts Updated sidebar to show logout/edit profile options

--- client/components/sections/SidebarSideSection.js
+++ client/components/sections/SidebarSideSection.js
@@ -6,12 +6,13 @@ const SidebarSideSection = (props) => <div class="sidebarSideSection">
         <li><a href=""><i class="fa fa-bell-o"></i></a></li>
         <li><a href=""><i class="fa fa-clock-o"></i></a></li>
     </ul>
-    <ul class="dropdown list-unstyled bottom-option">
+    <ul class="list-unstyled bottom-option">
         <li><a href=""><i class="fa fa-cog"></i></a></li>
-        <li class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown"><a href=""><i class="fa fa-user-o"></i></a>
-            <ul class="dropdown-menu">
-                <li><a href="#">Sub Item 1</a></li>
-                <li><a href="#">Sub Item 2</a></li>
+        <li class="sub-menu-parent" tab-index="0"><a href=""><i class="fa fa-user-o"></i></a>
+            <ul class="sub-menu">
+                <i class="down-arrow"></i>
+                <li><a href="">Edit Profile</a></li>
+                <li><a href="/logout">Logout</a></li>
             </ul>
         </li>
     </ul>
