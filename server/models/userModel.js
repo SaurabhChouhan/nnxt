fd93b2ea0b3cf3612c83b30b96f6c72e9c9f01db
Saurabh Chouhan <schouhan@aripratech.com>
2018-01-23 18:15:24 +0530

    Changed bcrypt to bcryptjs

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import _ from 'lodash'
 import * as ErrorCodes from '../errorcodes'
 import AppError from '../AppError'
