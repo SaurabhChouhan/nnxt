e0223b55fbeb27f1469978ead25206b2b46ccda3
Saurabh Chouhan <schouhan@aripratech.com>
2018-01-26 00:25:28 +0530

    Added tcomb doc to allow user to see valid input for any API

--- server/routers/estimation-router.js
+++ server/routers/estimation-router.js
@@ -4,12 +4,15 @@ import {hasRole} from "../utils"
 import {ROLE_NEGOTIATOR} from "../serverconstants";
 import {ACCESS_DENIED, HTTP_FORBIDDEN} from "../errorcodes"
 import AppError from '../AppError'
+import {toObject} from 'tcomb-doc'
+import {estimationInitiationStruct} from "../validation/estimation-validation"
 
 let estimationRouter = new Router({
     prefix: "estimations"
 })
 
 estimationRouter.post('/initiate', async ctx => {
+    //return toObject(estimationInitiationStruct)
     if (!hasRole(ctx, ROLE_NEGOTIATOR))
         throw new AppError("Only users with role [Negotiator] can initiate estimation", ACCESS_DENIED, HTTP_FORBIDDEN)
     return EstimationModel.initiate(ctx.request.body, ctx.state.user)
