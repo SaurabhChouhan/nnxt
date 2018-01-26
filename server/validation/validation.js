8f56480254a1edb6e894c67f79f16954938cf613
Saurabh Chouhan <schouhan@aripratech.com>
2018-01-26 14:50:53 +0530

    Added new type for validating mongodb ObjectID Added appropriate custom error messages against each rule

--- server/validation/validation.js
+++ server/validation/validation.js
@@ -1,6 +1,7 @@
 import t from 'tcomb-validation'
 import {API_VALIDATION_FAILED, HTTP_BAD_REQUEST} from "../errorcodes"
 import _ from 'lodash'
+import mongoose from 'mongoose'
 
 /**
  * Required string rule
@@ -16,12 +17,25 @@ t.String.getValidationErrorMessage = (value, path, context) => {
 export {default as generateSchema} from './generateSchema'
 
 export let RequiredString = t.refinement(t.String, s => _.trim(s).length > 0, "String")
-/*
+
+export let ObjectId = t.refinement(t.String, s => mongoose.Types.ObjectId.isValid(s), "ObjectID")
+
+t.String.getValidationErrorMessage = (value, path, context) => {
+    if(!value)
+        return "Field is required"
+    else if(typeof(value) != 'string')
+        return "Value ["+value+"] not a valid string"
+}
+
 RequiredString.getValidationErrorMessage = (value, path, context) => {
     if (!value || _.trim(value).length == 0)
-        return path.join("/") + ' is Required'
+        return "A non-empty value is required"
 }
-*/
+
+ObjectId.getValidationErrorMessage = (value, path, context) => {
+    return "Value ["+value+"] is not a valid Mongo Object ID"
+}
+
 
 class ValidationError extends Error {
     constructor(message, errors) {
