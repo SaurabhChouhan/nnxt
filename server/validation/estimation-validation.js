01f1d2de60a8220ee430905ed9b96fd809e40f0f
Saurabh Chouhan <schouhan@aripratech.com>
2018-01-26 14:52:18 +0530

    Modified to use Object ID validation

--- server/validation/estimation-validation.js
+++ server/validation/estimation-validation.js
@@ -1,13 +1,14 @@
-import {RequiredString, validate} from "./index"
+import {RequiredString, ObjectId, validate} from "./index"
 import t from 'tcomb-validation'
 
-export let estimationInitiationStruct = t.struct({
+export const estimationInitiationStruct = t.struct({
     description: RequiredString,
     estimator: t.struct({
-        _id: RequiredString
+        _id: ObjectId
     }),
     project: t.struct({
-        _id: RequiredString
+        _id: ObjectId
     }),
     technologies: t.maybe(t.list(t.String))
 })
+
