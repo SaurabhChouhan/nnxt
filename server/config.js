758e431921c9f379cb3ea77536d0c09db4f60134
mpogra <apogra@gmail.com>
2018-01-30 11:46:33 +0530

    fixed comma

--- server/config.js
+++ server/config.js
@@ -31,7 +31,7 @@ const config = {
             dbname: 'nnxt-gaurav',
             url: 'mongodb://gaurav1:gaurav1@ds213338.mlab.com:13338/nnxt-gaurav',
             useMongoClient: true
-        }
+        },
         pogras: {
             dbname: 'nnxt-pogras',
             url: 'mongodb://mpogra:mpogra@ds117848.mlab.com:17848/nnxt-pogras',
