f627628165686b292ace90a46f5846cd79d85a1a
Gaurav Agrawal <gaurav.aripra@gmail.com>
2018-02-07 19:08:28 +0530

    - Formated Reducer

--- client/reducers/estimationReducer.js
+++ client/reducers/estimationReducer.js
@@ -9,6 +9,7 @@ let initialState = {
 
 const estimationReducer = (state = initialState, action) => {
     switch (action.type) {
+
         case AC.ADD_ESTIMATION_TASK:
             // task is added to estimation, it would be added against selected estimation
             return Object.assign({}, state, {
@@ -26,11 +27,13 @@ const estimationReducer = (state = initialState, action) => {
             return Object.assign({}, state, {
                 features: Array.isArray(state.features) ? [...state.features, action.feature] : [action.feature]
             })
+
         case AC.UPDATE_ESTIMATION_FEATURE:
             // feature is added to estimation, it would be added against selected estimation
             return Object.assign({}, state, {
                 features: Array.isArray(state.features) ? state.features.map(item => item._id == action.feature._id ? action.feature : item) : null
             })
+
         case AC.ESTIMATION_TASK_DELETE:
             return Object.assign({}, state, {
                 tasks: state.tasks.map(t => {
@@ -40,6 +43,7 @@ const estimationReducer = (state = initialState, action) => {
                     return t
                 })
             })
+
         case AC.RQUEST_FOR_TASK_EDIT_PERMISSION:
             return Object.assign({}, state, {tasks: state.tasks.map(item => item._id == action.task._id ? action.task : item)})
 
@@ -48,10 +52,13 @@ const estimationReducer = (state = initialState, action) => {
 
         case AC.ADD_ESTIMATIONS:
             return Object.assign({}, state, {all: action.estimations})
+
         case AC.ADD_ESTIMATION:
             return Object.assign({}, state, {all: [...state.all, action.estimation]})
+
         case AC.EDIT_ESTIMATION:
             return Object.assign({}, state, {all: state.all.map(item => item._id == action.estimation._id ? action.estimation : item)})
+
         case AC.SELECT_ESTIMATION:
             return Object.assign({}, state, {
                 selected: Object.assign({}, action.estimation, {
@@ -61,6 +68,7 @@ const estimationReducer = (state = initialState, action) => {
                 tasks: [...action.estimation.tasks],
                 features: [...action.estimation.features]
             })
+        
         default:
             return state
     }
