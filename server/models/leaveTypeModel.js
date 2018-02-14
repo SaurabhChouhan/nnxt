6e6a72101b4abb7ba8b9f73bf047b64b8442124f
Murtaza <murtaza.aripra@gmail.com>
2018-02-14 12:48:47 +0530

    -added another model to get the type of leave from server. -added get api in leave router. -added file in index.js

---
+++ server/models/leaveTypeModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+import AppError from '../AppError'
+import * as ErrorCodes from '../errorcodes'
+import {HTTP_BAD_REQUEST} from "../errorcodes";
+import {NOT_FOUND} from "../errorcodes";
+import {ALREADY_EXISTS} from "../errorcodes";
+
+mongoose.Promise = global.Promise
+
+let leaveTypesSchema = mongoose.Schema({
+    name: {
+        type: String, required: [true, 'Leave type is required']
+    },
+    description: {
+        type: String, required: [true, 'Leave description is required']
+    },
+    isDeleted: {type: Boolean, default: false},
+    isActive: {type: Boolean, default: true},
+    canHardDelete: {type: Boolean, default: false}
+})
+
+leaveTypesSchema.statics.getAllActiveLeaveTypes = async () => {
+    return await LeaveTypeModel.find({isDeleted: false, isActive: true})
+}
+
+
+const LeaveTypeModel = mongoose.model("leaveType", leaveTypesSchema)
+export default LeaveTypeModel
