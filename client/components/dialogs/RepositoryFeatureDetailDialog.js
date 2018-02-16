9e5c30e9519193ef9b3e768e8d9ec88df7f7dae0
Praveen Malakar <praveenmalakar1996@gmail.com>
2018-02-16 19:33:19 +0530

    - updated #277

--- client/components/dialogs/RepositoryFeatureDetailDialog.js
+++ client/components/dialogs/RepositoryFeatureDetailDialog.js
@@ -3,7 +3,7 @@ import React from 'react'
 import {RepositoryFeatureDetailPageContainer} from "../../containers"
 
 const RepositoryFeatureDetailDialog = (props) => {
-    return <Modal  className="estimationModal suggestDialog" show={props.show} onHide={props.close}>
+    return <Modal  className="estimationModal" show={props.show} onHide={props.close}>
         <ModalHeader closeButton>
             <div className="clearfix ModalHeading">
                 <div className="col-md-1 ModalSideLabel"></div>
--- client/components/pages/RepositoryFeatureDetailPage.js
+++ client/components/pages/RepositoryFeatureDetailPage.js
@@ -3,11 +3,20 @@ import React, {Component} from 'react'
 class RepositoryFeatureDetailPage extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            showHistory: false
+        }
+        this.showHistoryDetail = this.showHistoryDetail.bind(this);
+    }
+
+    showHistoryDetail() {
+        this.setState({showHistory: !this.state.showHistory})
     }
 
 
     render() {
         const {feature} = this.props
+        const {showHistory} = this.state
         return (
             <div className="col-md-12">
                 <div className="col-md-6">
@@ -17,23 +26,23 @@ class RepositoryFeatureDetailPage extends Component {
                     <h3>{feature.estimatedHours ? '(' + feature.estimatedHours + ')' : '(00)'} </h3>
                 </div>
                 <div className="col-md-12">
-                    <p>Feature Description: {feature.description} </p>
+                    <p className="repositoryModalPara">Feature Description: {feature.description} </p>
                 </div>
-                <div className="col-md-12">
-                    <div className="col-md-2">
-                        <button type="button" onClick={
+                <div className="col-md-12 pad">
+                    <div className="col-md-3">
+                        <button className="customBtn" type="button" onClick={
                             this.showHistoryDetail
                         }>History
                         </button>
                     </div>
-                    <div className="col-md-4">
-                        <button type="button" onClick={() => {
+                    <div className="col-md-4 pad">
+                        <button className="customBtn" type="button" onClick={() => {
                             this.props.addFeature(feature._id, feature.estimation._id)
                         }}>Add to Estimation
                         </button>
                     </div>
-                    <div className="col-md-4">
-                        <button type="button">Copy To Estimation</button>
+                    <div className="col-md-5">
+                        <button className="customBtn" type="button">Copy To Estimation</button>
                     </div>
                 </div>
                 {showHistory &&
