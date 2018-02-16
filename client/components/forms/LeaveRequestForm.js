36cf3f7bfff21b6535d6bb8717a94949ff6e13df
Ratnesh Jain <ratnesh59@gmail.com>
2018-02-16 16:48:28 +0530

    Date Picker changes in leave request page.

--- client/components/forms/LeaveRequestForm.js
+++ client/components/forms/LeaveRequestForm.js
@@ -3,6 +3,9 @@ import {renderDateTimePicker, renderDateTimePickerString, renderSelect, renderTe
 import {Field, reduxForm} from 'redux-form'
 import React from 'react'
 import moment from 'moment'
+import momentLocalizer from 'react-widgets-moment'
+moment.locale('en')
+momentLocalizer()
 import * as logger from "../../clientLogger";
 
 let LeaveRequestForm = (props) => {
@@ -15,11 +18,13 @@ let LeaveRequestForm = (props) => {
                 <Field name="user.user.firstName" component="input" type="hidden"/>
                 <Field name="user.user.lastName" component="input" type="hidden"/>
 
-                <Field name="leave.from" placeholder={"Leave From :"} component={renderText}
-                       label={"From :"} validate={[required]}/>
+                <Field name="leave.from" placeholder={"Leave From :"} component={renderDateTimePicker}
+                       min={moment()}
+                       showTime={false} label={"From :"} validate={[required]}/>
 
-                <Field name="leave.to" placeholder={"Leave To :"} component={renderText}
-                       label={"To :"} validate={[required]}/>
+                <Field name="leave.to" placeholder={"Leave To :"} component={renderDateTimePicker}
+                       min={moment()}
+                       showTime={false} label={"To :"} validate={[required]}/>
 
                 <Field name="leave.type" placeholder={"type of leave"} displayField={"name"} valueField={"name"} component={renderSelect} options={props.leaveTypes}
                        label={"Type :"} validate={[required]}/>
