1b2232b84f4b069f7ee9dbe943275f02f31c133d
Gaurav Agrawal <gaurav.aripra@gmail.com>
2018-02-16 15:47:59 +0530

    - Added dialog page for feature page

--- client/reducers/repositoryReducer.js
+++ client/reducers/repositoryReducer.js
@@ -10,8 +10,13 @@ const repositoryReducer = (state = initialState, action) => {
     switch (action.type) {
         case AC.SELECT_REPOSITORY:
             return Object.assign({}, state, {all: action.repository})
+
         case AC.SELECT_TASK_FROM_REPOSITORY:
             return Object.assign({}, state, {task: action.task})
+
+        case AC.SELECT_FEATURE_FROM_REPOSITORY:
+            return Object.assign({}, state, {feature: action.feature})
+
         default:
             return state
     }
