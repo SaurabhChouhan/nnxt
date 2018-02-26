1323ee9547c73c9907142a08ab5da1bf28e4397d
Gaurav Agrawal <gaurav.aripra@gmail.com>
2018-02-26 15:30:13 +0530

    - Added component and Container to it

---
+++ client/components/pages/ReleaseTaskDetailPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Header from '../sections/Header'
+import {LoginFormContainer} from '../../containers'
+
+class ReleaseTaskDetailPage extends Component {
+    render() {
+        return ([<h1>hello world</h1>])
+    }
+}
+
+export default ReleaseTaskDetailPage
