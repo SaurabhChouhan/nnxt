287f5a0f9ab551f08aa2bd185db8bb7ac9c8ae49
Gaurav Agrawal <gaurav.aripra@gmail.com>
2018-02-28 18:55:18 +0530

    - Added selector and call for action call

--- client/components/forms/ReleaseDeveloperFilterForm.js
+++ client/components/forms/ReleaseDeveloperFilterForm.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react'
-import {Field, reduxForm} from 'redux-form'
-import {renderDateTimePicker, renderSelect, renderText} from './fields'
-import {number, required} from "./validation"
+import {Field, formValueSelector, reduxForm} from 'redux-form'
+import {renderDateTimePicker, renderSelect,} from './fields'
 import moment from 'moment'
 import momentLocalizer from 'react-widgets-moment'
+import {connect} from 'react-redux'
 
 moment.locale('en')
 momentLocalizer()
 let ReleaseTaskPlanningForm = (props) => {
-    const {change, team, handleSubmit, reset} = props
+    const {change, team, handleSubmit, startDate, endDate} = props
 
     return <form onSubmit={handleSubmit}>
 
@@ -16,7 +16,7 @@ let ReleaseTaskPlanningForm = (props) => {
             <Field name="employee._id" placeholder={"Name of Developer"}
                    onChange={(event, newValue, oldValue) =>
                    {
-                       this.props.getDeveloperDetails(newValue,"","")
+                       props.getDeveloperDetails(newValue, startDate, endDate)
                    }}
                    component={renderSelect} options={team}
                    label={"Developer Name:"} />
@@ -24,12 +24,12 @@ let ReleaseTaskPlanningForm = (props) => {
         </div>
         <div className="col-md-8">
             <div className="col-md-6">
-                <Field name="planningDate" placeholder={"Date"} component={renderDateTimePicker}
+                <Field name="startDate" placeholder={"Start Date"} component={renderDateTimePicker}
                        showTime={false}
                        label={" From :"} />
             </div>
             <div className="col-md-6">
-                <Field name="planningDate" placeholder={"Date"} component={renderDateTimePicker}
+                <Field name="endDate" placeholder={" End Date"} component={renderDateTimePicker}
                        showTime={false}
                        label={" To :"}/>
             </div>
@@ -41,5 +41,18 @@ let ReleaseTaskPlanningForm = (props) => {
 ReleaseTaskPlanningForm = reduxForm({
     form: 'task-planning'
 })(ReleaseTaskPlanningForm)
+
+const selector = formValueSelector('task-planning')
+
+ReleaseTaskPlanningForm = connect(
+    state => {
+        const {startDate, endDate} = selector(state, 'startDate', 'endDate')
+        return {
+            startDate,
+            endDate
+        }
+    }
+)(ReleaseTaskPlanningForm)
+
 
 export default ReleaseTaskPlanningForm
--- client/containers/forms/ReleaseDeveloperFilterFormContainer.js
+++ client/containers/forms/ReleaseDeveloperFilterFormContainer.js
@@ -1,10 +1,9 @@
 import {connect} from 'react-redux'
 import {ReleaseDeveloperFilterForm} from "../../components"
 import * as A from "../../actions"
-import * as COC from "../../components/componentConsts";
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-    getDeveloperDetails :(developerId,StartDate,EndDate)=>console.log("developer value",developerId,StartDate,EndDate)
+    getDeveloperDetails: (developerId, StartDate, EndDate) => console.log("developer value developerId ", developerId, "Start Date ", StartDate, "End Date ", EndDate)
 })
 
 const mapStateToProps = (state, ownProps) => ({
