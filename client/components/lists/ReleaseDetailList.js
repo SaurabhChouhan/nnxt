fa4fbe045840547f6e63f7c27c8f451ecfb8fc32
Khushboo Mishra <khushboo.aripra@gmail.com>
2018-03-30 15:50:25 +0530

    task details list search button

--- client/components/lists/ReleaseDetailList.js
+++ client/components/lists/ReleaseDetailList.js
@@ -12,6 +12,22 @@ class ReleaseDetailList extends Component {
         this.options = {
             onRowClick: this.onRowClick.bind(this)
         }
+        this.state = {
+            status: "all",
+            flag: "all"
+        }
+        this.onFlagChange = this.onFlagChange.bind(this)
+        this.onStatusChange = this.onStatusChange.bind(this)
+
+    }
+    onFlagChange(flag) {
+        this.setState({flag: flag})
+        this.props.changeReleaseFlag(this.props.release, this.state.status, flag)
+    }
+
+    onStatusChange(status) {
+        this.setState({status: status})
+        this.props.changeReleaseStatus(this.props.release, status, this.state.flag)
     }
 
     onRowClick(row) {
@@ -126,7 +142,40 @@ class ReleaseDetailList extends Component {
                 <div className="col-md-12">
                     <div className="col-md-12 releaseOption">
 
-                        <ReleaseTaskSearchFormContainer/>
+                        <div className="col-md-3">
+                            <div>
+                                <select className="form-control" onChange={(flag) =>
+                                    this.onFlagChange(flag.target.value)
+                                }>
+                                    <option value="all">All Flags</option>
+                                    <option value={SC.FLAG_UNPLANNED}>{SC.FLAG_UNPLANNED}</option>
+                                    <option value={SC.FLAG_EMPLOYEE_ON_LEAVE}>{SC.FLAG_EMPLOYEE_ON_LEAVE}</option>
+                                    <option value={SC.FLAG_DEV_DATE_MISSED}>{SC.FLAG_DEV_DATE_MISSED}</option>
+                                    <option value={SC.FLAG_HAS_UNREPORTED_DAYS}>{SC.FLAG_HAS_UNREPORTED_DAYS}</option>
+                                    <option
+                                        value={SC.FLAG_PENDING_AFTER_END_DATE}>{SC.FLAG_PENDING_AFTER_END_DATE}</option>
+                                    <option
+                                        value={SC.FLAG_COMPLETED_BEFORE_END_DATE}>{SC.FLAG_COMPLETED_BEFORE_END_DATE}</option>
+
+                                </select>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div>
+                                <select className="form-control"
+                                        onChange={(status) => this.onStatusChange(status.target.value)}>
+                                    <option value="all">All Status</option>
+                                    <option value={SC.STATUS_UNPLANNED}>{SC.STATUS_UNPLANNED}</option>
+                                    <option value={SC.STATUS_PENDING}>{SC.STATUS_PENDING}</option>
+                                    <option value={SC.STATUS_DEV_IN_PROGRESS}>{SC.STATUS_DEV_IN_PROGRESS}</option>
+                                    <option value={SC.STATUS_DEV_COMPLETED}>{SC.STATUS_DEV_COMPLETED}</option>
+                                    <option value={SC.STATUS_RELEASED}>{SC.STATUS_RELEASED}</option>
+                                    <option value={SC.STATUS_ISSUE_FIXING}>{SC.STATUS_ISSUE_FIXING}</option>
+                                    <option value={SC.STATUS_OVER}>{SC.STATUS_OVER}</option>
+
+                                </select>
+                            </div>
+                        </div>
                     </div>
                     <div className="col-md-12 releaseOption">
                         <div className="col-md-2 pad ">
@@ -159,6 +208,8 @@ class ReleaseDetailList extends Component {
                     </div>
                     <div className="estimation">
                         <BootstrapTable options={this.options} data={this.props.releasePlans}
+                                        multiColumnSearch={true}
+                                        search={true}
                                         striped={true}
                                         hover={true}>
                             <TableHeaderColumn columnTitle isKey dataField='_id' hidden={true}>ID</TableHeaderColumn>
