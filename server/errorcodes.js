06d13a37a6babcbfeec9d93fcd29adae4938c8e8
Gaurav Agrawal <gaurav.aripra@gmail.com>
2018-04-27 18:25:16 +0530

    - Formated file and added new router

--- server/errorcodes.js
+++ server/errorcodes.js
@@ -1,4 +1,5 @@
 export const ALREADY_EXISTS = "ALREADY_EXISTS"
+export const NOT_EXISTS = "NOT_EXISTS"
 export const NOT_FOUND = "NOT_FOUND"
 export const EMAIL_ALREADY_USED = "EMAIL_ALREADY_USED"
 export const ACCESS_DENIED = "ACCESS_DENIED"
