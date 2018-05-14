2a5cfe64b0845cefc7f6b9e5f860af0ef786237d
Khushboo Mishra <khushboo.aripra@gmail.com>
2018-05-14 18:03:02 +0530

    -Added color to show taskPlan status on calendar section

--- client/containers/pages/CalendarPageContainer.js
+++ client/containers/pages/CalendarPageContainer.js
@@ -4,24 +4,26 @@ import * as A from '../../actions'
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     return (
-    {
-        showSelectedTaskDetail: (event) => dispatch(A.showSelectedTaskDetail(event)),
-        changeViewAndDate:(view,date)=>{
-            dispatch(A.changeNavigationView(view,date))
-        },
-        showCalendarView: (event) => dispatch(A.showCalendarView()),
-    }
-)}
+        {
+            showSelectedTaskDetail: (event) => dispatch(A.showSelectedTaskDetail(event)),
+            changeViewAndDate: (view, date) => {
+                dispatch(A.changeNavigationView(view, date))
+            },
+            showCalendarView: (event) => dispatch(A.showCalendarView()),
+        }
+    )
+}
 
 const mapStateToProps = (state) => {
     return {
-    calendar: state.calendar.loggedInUser,
-    events: state.calendar.events,
-    visibility: state.calendar.visibility,
-    selectedTaskDetail: state.calendar.selectedTaskDetail,
-    selectedView:state.calendar.selectedView,
-    selectedDate:state.calendar.selectedDate
-}}
+        calendar: state.calendar.loggedInUser,
+        events: state.calendar.events,
+        visibility: state.calendar.visibility,
+        selectedTaskDetail: state.calendar.selectedTaskDetail,
+        selectedView: state.calendar.selectedView,
+        selectedDate: state.calendar.selectedDate
+    }
+}
 
 const CalendarPageContainer = connect(
     mapStateToProps,
