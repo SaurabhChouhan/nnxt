d23ec5dc6850b1952433071c0ccae6d84fdc17f1
Gaurav Agrawal <gaurav.aripra@gmail.com>
2018-05-16 15:28:21 +0530

    - Added validation for employees

---
+++ server/validation/employee-validation.js
@@ -0,0 +1,21 @@
+import {ObjectId, RequiredString} from "./index"
+import t from 'tcomb-validation'
+
+export const employeeAddEmployeeDaysStruct = t.struct({
+    _id: t.Nil,
+    project: t.struct({
+        _id: ObjectId,
+        name: t.String
+    }),
+    employee: t.struct({
+        _id: ObjectId,
+        name: t.String
+    }),
+    plannedHours: t.Number,
+    dateString: RequiredString,
+})
+
+export const employeeUpdateEmployeeDaysStruct = t.struct({
+    plannedHours: t.Number,
+    dateString: RequiredString,
+})
