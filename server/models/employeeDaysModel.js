297e15a56d0bb51052899b3bc683eaea63c97211
Gaurav Agrawal <gaurav.aripra@gmail.com>
2018-05-16 15:41:51 +0530

    - updated employee days model with add data and update data

--- server/models/employeeDaysModel.js
+++ server/models/employeeDaysModel.js
@@ -1,4 +1,8 @@
 import mongoose from 'mongoose'
+import * as V from '../validation'
+import * as EC from '../errorcodes'
+import * as SC from '../serverconstants'
+import momentTZ from 'moment-timezone'
 
 mongoose.Promise = global.Promise
 
@@ -18,8 +22,48 @@ let employeeDaysSchema = mongoose.Schema({
 
 
 employeeDaysSchema.statics.addEmployeeDaysDetails = async (EmployeeDaysInput, user) => {
+    V.validate(EmployeeDaysInput, V.employeeAddEmployeeDaysStruct)
+    let momentEmployeeDate = momentTZ.tz(EmployeeDaysInput.dateString, SC.DATE_FORMAT, SC.DEFAULT_TIMEZONE).clone().hour(0).minute(0).second(0).millisecond(0)
+    EmployeeDaysInput.date = momentEmployeeDate
+
+    let countDate = await EmployeeDaysModel.count({"date": EmployeeDaysInput.date})
+    if (countDate > 0) {
+        throw new AppError('Employee days detail is already available on this date ' + EmployeeDaysInput.date + ' can not create another', EC.ALREADY_EXISTS, EC.HTTP_BAD_REQUEST)
+    }
     return await EmployeeDaysModel.create(EmployeeDaysInput)
 }
+
+employeeDaysSchema.statics.increasePlannedHoursOnEmployeeDaysDetails = async (EmployeeDaysInput, user) => {
+    V.validate(EmployeeDaysInput, V.employeeUpdateEmployeeDaysStruct)
+    let momentEmployeeDate = momentTZ.tz(EmployeeDaysInput.dateString, SC.DATE_FORMAT, SC.DEFAULT_TIMEZONE).clone().hour(0).minute(0).second(0).millisecond(0)
+    EmployeeDaysInput.date = momentEmployeeDate
+    let count = await EmployeeDaysModel.count({"date": EmployeeDaysInput.date})
+    if (count <= 0) {
+        throw new AppError('Employee days detail is not available on this date ' + EmployeeDaysInput.date + ' can not update ', EC.ALREADY_EXISTS, EC.HTTP_BAD_REQUEST)
+    }
+    return await EmployeeDaysModel.update({
+        "date": EmployeeDaysInput.date
+    }, {
+        $inc: {"plannedHours": EmployeeDaysInput.plannedHours}
+    })
+}
+
+employeeDaysSchema.statics.decreasePlannedHoursOnEmployeeDaysDetails = async (EmployeeDaysInput, user) => {
+    let momentEmployeeDate = momentTZ.tz(EmployeeDaysInput.dateString, SC.DATE_FORMAT, SC.DEFAULT_TIMEZONE).clone().hour(0).minute(0).second(0).millisecond(0)
+    EmployeeDaysInput.date = momentEmployeeDate
+
+    V.validate(EmployeeDaysInput, V.employeeUpdateEmployeeDaysStruct)
+    let count = await EmployeeDaysModel.count({"date": EmployeeDaysInput.date})
+    if (count <= 0) {
+        throw new AppError('Employee days detail is not available on this date ' + EmployeeDaysInput.date + ' can not update ', EC.ALREADY_EXISTS, EC.HTTP_BAD_REQUEST)
+    }
+    return await EmployeeDaysModel.update({
+        "date": EmployeeDaysInput.date
+    }, {
+        $inc: {"plannedHours": -EmployeeDaysInput.plannedHours}
+    })
+}
+
 employeeDaysSchema.statics.getActiveEmployeeDays = async (user) => {
     return await EmployeeDaysModel.find({})
 }
