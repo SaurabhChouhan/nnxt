8d2770f5c7da0e426e34ba3e2acb24beabbee73b
Khushboo Mishra <khushboo.aripra@gmail.com>
2018-06-20 16:45:08 +0530

    - Fixed Conditions of leave detail page

--- client/components/pages/LeaveDetailPage.js
+++ client/components/pages/LeaveDetailPage.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import moment from 'moment'
 import {Timeline, TimelineEvent} from 'react-event-timeline'
+import * as SC from '../../../server/serverconstants'
+import _ from 'lodash'
 import {withRouter} from 'react-router-dom'
 
 class LeaveDetailPage extends Component {
@@ -11,6 +13,7 @@ class LeaveDetailPage extends Component {
     render() {
         const {leave} = this.props
         console.log("leave", leave)
+        var approval = leave && leave.status && ( leave.status === SC.LEAVE_STATUS_APPROVED || leave.status === SC.LEAVE_STATUS_CANCELLED )
         return (
             <Timeline>
                 <span>
@@ -22,67 +25,92 @@ class LeaveDetailPage extends Component {
                         <i className="glyphicon glyphicon-arrow-left"></i></button>
                 </span>
                 <TimelineEvent style={{fontSize: '20px'}}
-                title={'Created Date:'}
-                icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
+                               title={'Created Date:'}
+                               icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
 
-                {leave && leave.created ? moment(leave.created).format("DD-MM-YYYY hh:mm:ss") : ''}
+                    {leave && leave.created ? moment(leave.created).format("DD-MM-YYYY hh:mm:ss") : ''}
                 </TimelineEvent>
 
                 <TimelineEvent style={{fontSize: '20px'}}
-                title={'Start Date:'}
-                icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
+                               title={'Start Date:'}
+                               icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
 
-                {leave && leave.startDate ? moment(leave.startDate).format("DD-MM-YYYY hh:mm:ss") : ''}
+                    {leave && leave.startDateString && !_.isEmpty(leave.startDateString) ? leave.startDateString : ''}
                 </TimelineEvent>
 
                 <TimelineEvent style={{fontSize: '20px'}}
-                title={'End Date:'}
-                icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
+                               title={'End Date:'}
+                               icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
 
-                {leave && leave.endDate ? moment(leave.endDate).format("DD-MM-YYYY hh:mm:ss") : ''}
+                    {leave && leave.endDateString && !_.isEmpty(leave.endDateString) ? leave.endDateString : ''}
                 </TimelineEvent>
 
                 <TimelineEvent style={{fontSize: '20px'}}
-                title={'Leave Day Type:'}
-                icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
+                               title={'Total Leave Days:'}
+                               icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
 
-                {leave && leave.dayType ? leave.dayType : ''}
+                    {leave && leave.numberOfLeaveDays ? leave.numberOfLeaveDays : ''}
+                </TimelineEvent>
+
+
+                <TimelineEvent style={{fontSize: '20px'}}
+                               title={'Leave Day Type:'}
+                               icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
+                    {leave && leave.dayType ? leave.dayType : ''}
+                </TimelineEvent>
+
+                <TimelineEvent style={leave.status === SC.LEAVE_STATUS_APPROVED ? {
+                    color: "green",
+                    fontSize: '20px'
+                } : leave.status === SC.LEAVE_STATUS_CANCELLED ? {color: "red", fontSize: '20px'} : {fontSize: '20px'}}
+                               title={'Leave Status:'}
+                               icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
+
+                    {leave && leave.status ? leave.status : ''}
                 </TimelineEvent>
 
                 <TimelineEvent style={{fontSize: '20px'}}
-                title={'Leave Status:'}
-                icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
+                               title={'Leave Raised By:'}
+                               icon={<i className="glyphicon glyphicon-user calendar_icon"></i>}>
 
-                {leave && leave.status ? leave.status : ''}
+                    {leave && leave.user && leave.user.firstName ? leave.user.firstName + " " + leave.user.lastName : ''}
                 </TimelineEvent>
 
                 <TimelineEvent style={{fontSize: '20px'}}
-                title={'Leave Raised By:'}
-                icon={<i className="glyphicon glyphicon-user calendar_icon"></i>}>
+                               title={'Leave Type:'}
+                               icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
 
-                {leave && leave.user && leave.user.name ? leave.user.name : ''}
+                    {leave && leave.leaveType && leave.leaveType.name ? leave.leaveType.name : ''}
                 </TimelineEvent>
+
 
                 <TimelineEvent style={{fontSize: '20px'}}
-                title={'Leave Type:'}
-                icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
+                               title={'Leave Description:'}
+                               icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
 
-                {leave && leave.leaveType && leave.leaveType.name ? leave.leaveType.name : ''}
+                    {leave && leave.description ? leave.description : ''}
                 </TimelineEvent>
+                {approval ?
+                    <TimelineEvent title={'Leave Approval :'}
+                                   icon={<i className="glyphicon glyphicon-user calendar_icon"></i>}
+                                   style={{fontSize: '20px'}}>
+                        <Timeline>
+                            <TimelineEvent style={{fontSize: '20px'}}
+                                           title={leave.status === SC.LEAVE_STATUS_APPROVED ? 'APPROVED BY:' : "REJECTED BY:"}
+                                           icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
 
-                <TimelineEvent style={{fontSize: '20px'}}
-                title={'Total Leave Days:'}
-                icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
+                                {leave && leave.approver && leave.approver.name ? leave.approver.name : ''}
+                            </TimelineEvent>
+                            <TimelineEvent style={{fontSize: '20px'}}
+                                           title={'REASON:'}
+                                           icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
 
-                {leave && leave.numberOfLeaveDays  ? leave.numberOfLeaveDays : ''}
-                </TimelineEvent>
+                                {leave && leave.approver && leave.approver.reason ? leave.approver.reason : ''}
+                            </TimelineEvent>
+                        </Timeline>
+                    </TimelineEvent> : <span></span>
+                }
 
-                <TimelineEvent style={{fontSize: '20px'}}
-                title={'Leave Description:'}
-                icon={<i className="glyphicon glyphicon-tasks calendar_icon"></i>}>
-
-                {leave && leave.description  ? leave.description : ''}
-                </TimelineEvent>
             </Timeline>
 
 
