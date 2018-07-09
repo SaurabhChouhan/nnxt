7d1e83fd246d9b4236f03c6f24a68155704782c9
Gaurav Agrawal <gaurav.aripra@gmail.com>
2018-07-09 16:24:25 +0530

    - try to remove unique key warning

--- client/components/pages/AdminHome.js
+++ client/components/pages/AdminHome.js
@@ -4,7 +4,7 @@ import Tabs from '../sections/Tabs'
 
 class AdminHome extends Component {
     render() {
-        return ([<Header key="admin_header" {...this.props}/>,
+        return ([<Header key="admin_header1" {...this.props}/>,
             <div key="admin-home_div" className="col-md-12 pad topNav">
                 <Tabs match={this.props.match} {...this.props}/>
             </div>])
