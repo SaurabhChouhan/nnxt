32b6842bf103f6be62201c3ec322e52d0f2e5e10
Gaurav Agrawal <gaurav.aripra@gmail.com>
2018-07-23 15:05:39 +0530

    - Fixed release form conditions

--- client/components/forms/ReleasePlanAddToReleaseForm.js
+++ client/components/forms/ReleasePlanAddToReleaseForm.js
@@ -4,6 +4,7 @@ import {renderText, renderTextArea, renderSelect} from './fields'
 import {number, required} from "./validation"
 import moment from 'moment'
 import {withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
 import momentLocalizer from 'react-widgets-moment'
 import _ from 'lodash'
 import * as SC from '../../../server/serverconstants'
@@ -21,17 +22,18 @@ class ReleasePlanAddToReleaseForm extends Component {
     render() {
         const {pristine, submitting, change, reset} = this.props
         const {release, releasePlans, iterations, iteration_type} = this.props
+        console.log("iteration_type", iteration_type)
         return (<form onSubmit={this.props.handleSubmit}>
                 <div className="row">
 
                     <Field name="estimation._id" component="input" type="hidden"/>
                     <div className="col-md-12">
-                        {status === SC.ITERATION_TYPE_PLANNED && <div className="col-md-6">
+                        {iteration_type === SC.ITERATION_TYPE_PLANNED && <div className="col-md-6">
                             <Field name="billedHours" component={renderText} label={"Negotiated Billed Hours:"}
                                    validate={[required, number]}/>
                         </div>}
 
-                        {status === SC.ITERATION_TYPE_PLANNED && <div className="col-md-6">
+                        {iteration_type === SC.ITERATION_TYPE_PLANNED && <div className="col-md-6">
                             <Field name="estimatedHours" component={renderText}
                                    validate={[required, number]}
                                    label={"Estimated Hours:"}/>
@@ -60,8 +62,6 @@ class ReleasePlanAddToReleaseForm extends Component {
                                    valueField={"name"}
                             />
                         </div>
-
-
                     </div>
 
                 </div>
