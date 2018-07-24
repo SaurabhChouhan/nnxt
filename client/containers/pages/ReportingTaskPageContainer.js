240d5edab08f11de50fb8dab9bdea8e22c41ec24
Gaurav Agrawal <gaurav.aripra@gmail.com>
2018-07-24 16:46:06 +0530

    - changed name reportedstatus

--- client/containers/pages/ReportingTaskPageContainer.js
+++ client/containers/pages/ReportingTaskPageContainer.js
@@ -43,7 +43,7 @@ const mapStateToProps = (state, ownProps) => ({
     releases: state.report.availableReleases,
     dateOfReport: state.report.dateOfReport,
     releaseID: state.report.releaseID,
-    taskStatus: state.report.status
+    reportedStatus: state.report.reportedStatus
 })
 
 const ReportingTaskPageContainer = connect(
