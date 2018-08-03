864b01a2ab87ee8f5606a0dfe4882118da70fab0
Saurabh Chouhan <schouhan@aripratech.com>
2018-08-03 21:04:32 +0530

    Use for-of loop to run event one by one

--- server/eventhandlers/handlers.js
+++ server/eventhandlers/handlers.js
@@ -3,8 +3,47 @@ import {EventModel} from "../models";
 import momentTZ from "moment-timezone";
 import * as H from './handlers'
 
-export const generateUnreportedWarnings = (data) => {
-    console.log("Generating unreported warnings ", new Date())
+export const generateUnreportedWarnings = async (event, data) => {
+    console.log("generateUnreportedWarnings(): called with data ", data)
+    //console.log("generateUnreportedWarnings(): called with event ", event)
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(true)
+        }, 1000 * 20)
+    })
+}
+
+export const monthlyWorkSummaryEvent = async (event, data) => {
+    console.log("generateEmployeeMonthlyReport(): called with data ", data)
+    //console.log("generateEmployeeMonthlyReport(): called with event ", event)
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(true)
+        }, 1000 * 20)
+    })
+}
+
+export const processEvents = async events => {
+    let now = new Date()
+    for (const e of events) {
+        // Need to see if this event is indeed need to be execute by by comparing current date/time with time parsed using timezone
+        let eventMoment = momentTZ.tz(e.execution.dateString, e.execution.format, e.execution.timeZone)
+        if (eventMoment.isBefore(now)) {
+            console.log("Event with method [" + e.method + "] is eligible to be executed now")
+            // iterate on date array and create object from that
+            let data = {}
+            if (e.data && e.data.length) {
+                e.data.forEach(d => {
+                    data[d.key] = d.value
+                })
+            }
+            await H[e.method](e, data)
+
+        } else {
+            console.log("Event with method [" + e.method + "] is not eligible to be executed now")
+        }
+    }
 }
 
 export const executeEvents = async () => {
@@ -15,35 +54,8 @@ export const executeEvents = async () => {
         status: SC.EVENT_SCHEDULED
     })
 
-    console.log("h is ", H)
     console.log("found [" + events.length + "] events to run ")
     if (events && events.length) {
-        let now = new Date()
-        events.forEach(e => {
-            console.log("processing ", e)
-            // Need to see if this event is indeed need to be execute by by comparing current date/time with time parsed using timezone
-            let eventMoment = momentTZ.tz(e.execution.dateString, e.execution.format, e.execution.timeZone)
-            if (eventMoment.isBefore(now)) {
-                console.log("Event with method [" + e.method + "] is eligible to be executed now")
-                // iterate on date array and create object from that
-                let data = {}
-                if (e.data && e.data.length) {
-                    e.data.forEach(d => {
-                        data[d.key] = d.value
-                    })
-                }
-                console.log("Method [" + e.method + "] would be called with data as ", data)
-            } else {
-                console.log("Event with method [" + e.method + "] is not eligible to be executed now")
-            }
-        })
+        await processEvents(events)
     }
-
-    /*
-    await new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            resolve(1)
-        }, 1000 * 10)
-    })
-    */
 }
