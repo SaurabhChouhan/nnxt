4bb5326a99ea092859ec429a7d6ca11b6b374091
Saurabh Chouhan <schouhan@aripratech.com>
2018-09-02 15:25:03 +0530

    just testing

--- server/config.js
+++ server/config.js
@@ -9,8 +9,8 @@ const config = {
         },
         development: {
             port: 3000,
-            setupData: false,
-            dropDatabase: false
+            setupData: true,
+            dropDatabase: true
         },
         production: {
             port: 3000,
