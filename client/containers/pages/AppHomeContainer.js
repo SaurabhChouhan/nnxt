feafee9198ce813ad68da903c68ba22a01d2480a
Saurabh Chouhan <schouhan@aripratech.com>
2018-09-06 16:02:06 +0530

    Made changes to calendar colors Made calendar as home page

--- client/containers/pages/AppHomeContainer.js
+++ client/containers/pages/AppHomeContainer.js
@@ -1,13 +1,24 @@
 import {connect} from 'react-redux'
 import AppHome from '../../components/pages/AppHome'
 import {withRouter} from 'react-router-dom'
+import {showComponent} from "../../actions";
+import {CALENDAR_TASK_PAGE} from "../../components/componentConsts";
+import * as A from "../../actions";
 
 const mapStateToProps = (state) => ({
     loggedInUser: state.user.loggedIn
 })
 
+const mapDispatchToProps = (dispatch) => ({
+    showLaunchComponent: () => {
+        dispatch(A.getAllTaskPlansFromServer())
+        dispatch(showComponent(CALENDAR_TASK_PAGE))
+    }
+})
+
 const AppHomeContainer = withRouter(connect(
-    mapStateToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(AppHome))
 
 export default AppHomeContainer
