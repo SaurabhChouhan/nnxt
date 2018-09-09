69463c5db47857c6bb15c75d4e49372f45b428e9
Saurabh Chouhan <schouhan@aripratech.com>
2018-09-10 01:46:59 +0530

    2nd version still not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     module: {
         rules: [
@@ -7,7 +10,30 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {minimize: true}
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./server/views/index-tmpl.mustache",
+            filename: "./server/views/index.mustache"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
 };
