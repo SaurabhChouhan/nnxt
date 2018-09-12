8fb4c91c2ab0c10b728cf072141d4b39006fff5f
Saurabh Chouhan <schouhan@aripratech.com>
2018-09-12 14:19:49 +0530

    Added a new router that would be used to keep API responsible for updating database Added an API to update employee days to have reported hours for old task plans Modified company calendar to add planned/reported hours on hoer

--- server/routers/index.js
+++ server/routers/index.js
@@ -21,6 +21,7 @@ export {default as taskPlanRouter} from './taskplan-router'
 export {default as developmentTypeRouter} from './developmenttype-router'
 export {default as moduleRouter} from './module-router'
 export {default as releasePlanRouter} from './releaseplan-router'
+export {default as databaseChangesRouter} from './databasechanges-router'
 
 // Please export all routers above this router
 export {default as apiRouter} from './api-router'
