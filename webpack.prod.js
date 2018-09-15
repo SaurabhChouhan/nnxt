70ad3e231a4281d08258fdc66d7648d0a2df9e49
Saurabh Chouhan <schouhan@aripratech.com>
2018-09-15 20:10:31 +0530

    Moved production/dev configurations to separate files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,60 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = merge(common, {
+var publicPath = path.resolve(__dirname, 'public', 'dist')
+
+module.exports = {
     mode: 'production',
-});
+    entry: {
+        client: './client/client.js'
+    },
+    output: {
+        filename: 'js/[name].[chunkhash].bundle.js',
+        path: publicPath,
+        chunkFilename: 'js/[name].[chunkhash].bundle.js',
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {minimize: true}
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin([publicPath]),
+        new HtmlWebpackPlugin({
+            template: "./server/views/index-tmpl.mustache",
+            filename: path.resolve(publicPath, 'index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+};
