fba359b42629a81af1de9de404b95899faf2d6e3
Saurabh Chouhan <schouhan@aripratech.com>
2018-12-19 14:10:04 +0530

    Added migration steps in order to add release types information to employee days as well in order to differentiate between efforts spend in differet types of releases

--- server/routers/migration-router.js
+++ server/routers/migration-router.js
@@ -1,6 +1,8 @@
 import Router from 'koa-router'
 import * as MDL from "../models"
 import * as V from "../validation"
+import logger from '../logger'
+import * as U from '../utils'
 
 let migrationRouter = new Router({
     prefix: "/migration"
@@ -13,7 +15,7 @@ migrationRouter.get('/updateEmployeeDays', async ctx => {
     */
 
 
-    let releases = await MDL.ReleaseModel.find({name:'ongoing'})
+    let releases = await MDL.ReleaseModel.find()
 
     for(const release of releases){
         await processReleaseToUpdateEmployeeDays(release)
@@ -22,8 +24,72 @@ migrationRouter.get('/updateEmployeeDays', async ctx => {
     return ctx.body = 'success'
 })
 
-const processReleaseToUpdateEmployeeDays = (release) => {
-    console.log("processing release ["+release.name+"] found with type as ["+release.releaseType+"]")
+const processReleaseToUpdateEmployeeDays = async (release) => {
+    logger.info("processing release ["+release.name+"] found with type as ["+release.releaseType+"]")
+    let taskPlans = await MDL.TaskPlanningModel.find({'release._id':release._id}, {employee:true, planningDateString:true, 'planning.plannedHours':true, 'report.reportedHours':true})
+    // Iterate on each task plan and update employee days accordingly, it is assumed that initially employee days have be removed and would be created afresh
+    for(const taskPlan of taskPlans){
+        await processTaskPlanToUpdateEmployeeDays(taskPlan, release)
+    }
+}
+
+const processTaskPlanToUpdateEmployeeDays = async (taskPlan, release) => {
+    let employeeDay = await MDL.EmployeeDaysModel.findOne({'employee._id': taskPlan.employee._id, 'dateString': taskPlan.planningDateString})
+    logger.info('Processing task plan ', {taskPlan})
+    if(!employeeDay){
+        logger.info('No employee days found ', {employeeDay})
+        // need to create employee days
+        employeeDay = new MDL.EmployeeDaysModel()
+        employeeDay.plannedHours = taskPlan.planning.plannedHours
+
+        if(taskPlan.report && taskPlan.report.reportedHours){
+            employeeDay.reportedHours = taskPlan.report.reportedHours
+            employeeDay.releaseTypes = [{releaseType: release.releaseType, plannedHours: taskPlan.planning.plannedHours, reportedHours: taskPlan.report.reportedHours}]
+        } else {
+            employeeDay.releaseTypes = [{releaseType: release.releaseType, plannedHours: taskPlan.planning.plannedHours}]
+        }
+        
+        employeeDay.employee = {
+            _id: taskPlan.employee._id,
+            name: taskPlan.employee.name
+        }
+        employeeDay.dateString = taskPlan.planningDateString
+        employeeDay.date = U.momentInUTC(taskPlan.planningDateString)
+        await employeeDay.save()
+
+    } else {
+        // need to update employee days accordingly
+        logger.info('employee days found ', {employeeDay})
+        employeeDay.plannedHours += taskPlan.planning.plannedHours
+
+        if(taskPlan.report && taskPlan.report.reportedHours){
+            employeeDay.reportedHours += taskPlan.report.reportedHours
+        }
+
+        let typeIdx = employeeDay.releaseTypes.findIndex(s => s.releaseType == release.releaseType)
+        if (typeIdx > -1) {
+            employeeDay.releaseTypes[typeIdx].plannedHours += taskPlan.planning.plannedHours
+            if(taskPlan.report && taskPlan.report.reportedHours){
+              employeeDay.releaseTypes[typeIdx].reportedHours += taskPlan.report.reportedHours
+            }
+        } else {
+            if(taskPlan.report && taskPlan.report.reportedHours){
+                employeeDay.releaseTypes.push({
+                    releaseType: release.releaseType,
+                    plannedHours: taskPlan.planning.plannedHours,
+                    reportedHours:taskPlan.report.reportedHours
+                })
+            } else {
+                employeeDay.releaseTypes.push({
+                    releaseType: release.releaseType,
+                    plannedHours: taskPlan.planning.plannedHours
+                })
+            }
+        }
+
+        logger.info('updated employee days ', {employeeDay})
+        await employeeDay.save()
+    }
 }
 
 export default migrationRouter
