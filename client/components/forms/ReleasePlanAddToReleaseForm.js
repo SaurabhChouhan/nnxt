7b80f30999a52c1678957ba30e1a5f035489a9ae
HrituDubey <hritu.aripra@gmail.com>
2019-06-28 18:50:54 +0530

    -add a checkbox field addDayTask in ReleasePlanAddToReleaseForm. -when addDayTask is checked  then another three field planningDate, plannedHours, and employees dropdown.

--- client/components/forms/ReleasePlanAddToReleaseForm.js
+++ client/components/forms/ReleasePlanAddToReleaseForm.js
@@ -1,10 +1,11 @@
 import {Field, formValueSelector, reduxForm} from 'redux-form'
 import React, {Component} from 'react'
-import {renderText, renderTextArea, renderSelect} from './fields'
+import {renderText, renderTextArea, renderSelect, renderCheckBox, renderDateTimePickerString} from './fields'
 import {number, required} from "./validation"
 import moment from 'moment'
-import {withRouter} from 'react-router-dom'
 import {connect} from 'react-redux'
+import momentTZ from 'moment-timezone'
+import {withRouter} from 'react-router-dom'
 import momentLocalizer from 'react-widgets-moment'
 import _ from 'lodash'
 import * as SC from '../../../server/serverconstants'
@@ -22,8 +23,14 @@ class ReleasePlanAddToReleaseForm extends Component {
 
     render() {
         const {pristine, submitting, change, reset} = this.props
-        const {release, releasePlans, iterations, iteration_type} = this.props
+        const {release, releasePlans, iterations, iteration_type, addDayTask} = this.props
+        const devStartDateMoment = moment(momentTZ.utc(release.devStartDate).format(SC.DATE_FORMAT))
+        const devEndDateMoment = moment(momentTZ.utc(release.devEndDate).format(SC.DATE_FORMAT))
+        const min = devStartDateMoment.toDate()
+        const max = devEndDateMoment.toDate()
+        let employees =  release.team ;
         console.log("iteration_type", iteration_type)
+        console.log('addDayTaskValue', addDayTask)
         return (<form onSubmit={this.props.handleSubmit}>
                 <div className="row">
 
@@ -68,6 +75,54 @@ class ReleasePlanAddToReleaseForm extends Component {
                     </div>
                     <div className="col-md-12">
                         <div className="col-md-12">
+                            <Field name="addDayTask"
+                               type="checkbox"
+                               label="addDayTask"
+                               className="input checkbox planchk "
+                               component={renderCheckBox}/>
+                        </div>
+                    </div>
+                    {
+                        addDayTask?
+                    <div className="col-md-12">
+                        <div className='col-md-4'>
+                            <Field name="planningDate"
+                                   placeholder={"Date"}
+                                   component={renderDateTimePickerString}
+                                   showTime={false}
+                                   min={min}
+                                   max={max}
+                                   label={" Date :"} validate={[required]}/>
+                        </div>
+                        <div className='col-md-4'>
+                            <Field name="planning.plannedHours"
+                                   placeholder={"Enter Hours"}
+                                   component={renderSelect}
+                                   label={"Planning Hours:"}
+                                   validate={[required, number]}
+                                   options={
+                                       [{'name': '0.5'}, {'name': '1'}, {'name': '1.5'}, {'name': '2'}, {'name': '2.5'}, {'name': '3'}, {'name': '3.5'},
+                                           {'name': '4'}, {'name': '4.5'}, {'name': '5'}, {'name': '5.5'}, {'name': '6'}, {'name': '6.5'}, {'name': '7'},
+                                           {'name': '7.5'}, {'name': '8'}, {'name': '8.5'}, {'name': '9'}, {'name': '9.5'}, {'name': '10'}, {'name': '10.5'}
+                                           ,{'name': '11'}, {'name': '11.5'}, {'name': '12'}]}
+                                   valueField={"name"}
+                            />
+                        </div>
+                        <div className='col-md-4'>
+                            <Field name="employee._id" placeholder={"Name of Developer"}
+                                   component={renderSelect}
+                                   options={employees}
+                                   label={"Developer Name:"}
+                                   onChange={(event, newValue, oldValue) => {
+                                       // let employee = allTeam.find(e => e._id == newValue)
+                                       // change("employee.name", employee.name)
+                                   }}
+                                   validate={[required]}/>
+                        </div>
+                    </div>:null
+                    }
+                    <div className="col-md-12">
+                        <div className="col-md-12">
                             <Field name="name" component={renderText} label={"Name:"} validate={[required]}/>
 
                         </div>
@@ -107,8 +162,10 @@ const selector = formValueSelector('release-plan-add-to-release')
 ReleasePlanAddToReleaseForm = connect(
     state => {
         const iteration_type = selector(state, 'iteration_type')
+        const addDayTask = selector(state, 'addDayTask')
         return {
-            iteration_type
+            iteration_type,
+            addDayTask
         }
     }
 )(ReleasePlanAddToReleaseForm)
